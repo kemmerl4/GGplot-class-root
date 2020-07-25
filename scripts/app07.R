10ff9ad2bdade229c144c66248a58de233380d7a
kemmerl4 <kemmerl4@msu.edu>
2020-07-25 08:47:02 -0400

    lesson 7 lindsey

---
+++ scripts/app07.R
@@ -0,0 +1,55 @@
+# Application 7
+# 25 July 2020
+
+source( file="scripts/reference.R" )
+weatherData = read.csv( file="data/LansingNOAA2016-3.csv", 
+                        stringsAsFactors = FALSE )
+
+#create a vector that holds quantile values for .3 and .7
+relHumidityQuant = quantile(weatherData$relHum, probs=c(.30, .70))
+
+#Create three levels of humidity
+#create a new column called humidityLevel
+for(day in 1:nrow(weatherData)) # nrow(weatherData) = 366
+{
+  # if the value in relHum is less than or equal to the low quant value
+  if(weatherData$relHum[day] <= relHumidityQuant[1]) # <= 63
+  {
+    weatherData$humidityLevel[day] = "Low"
+  }
+  # if the value in relHum is greater than or equal to the high quant value
+  else if(weatherData$relHum[day] >= relHumidityQuant[2]) # >= 75
+  {
+    weatherData$humidityLevel[day] = "High"
+  }
+  else # the value in relHum is between 63 and 75
+  {
+    weatherData$humidityLevel[day] = "Medium"
+  }
+}
+
+windDirFact = factor(weatherData$windDir,
+                     levels=c("North", "East", "South", "West"))
+
+#Create a boxplot of stnPressure vs humidityLevel
+thePlot = ggplot(data=weatherData) +
+  geom_boxplot(mapping=aes(x=humidityLevel, y=stnPressure,
+                           fill=factor(windDir,
+                                       levels=c("North", "East", "South", "West"))),
+                           na.rm=TRUE) +
+  theme_bw() +
+  scale_x_discrete(limits = c("Low", "Medium", "High")) + #reorder humidityLevel
+  facet_grid(facets= . ~ factor(windSpeedLevel, #facet in horizantal direction
+                                levels=c("Low", "Medium", "High"))) + #reorder horiz facets
+  labs(title = "Pressure vs. Humidity Level",
+       subtitle = "Lansing, Michigan: 2016",
+       x = "Humidity Level",
+       y = "Pressure",
+       fill = "Wind Direction")
+plot(thePlot)
+
+
+
+
+
+
--- scripts/class_assign_20-July-2020.R
+++ scripts/class_assign_20-July-2020.R
@@ -2,23 +2,26 @@
   source(file="scripts/reference.R");  
   weatherData = read.csv(file="data/LansingNOAA2016.csv", 
                          stringsAsFactors = FALSE)
-}
   
 # grepl returns a true/false vector (l for logical)
-snow_rain_days = grepl(x=weatherData$weatherType, pattern="RA|SN");
+snow_rain_days = grepl(x=weatherData$weatherType, pattern="RA|SN")
   
 # for loop for 
   for(day in 1:nrow(weatherData))  
   {
-    if(snow_rain_days[day] == TRUE)  # day had either RA or SN
+    if(snow_rain_days[day] == TRUE) #if weatherType was RA rain or SN snow
     {
-      weatherData$snow_rain_days[day] = 1;   # set precip to 1
+      weatherData$snow_rain_days[day] = 1 #the value of snow_rain_days will be 1
     }
-    else   # day had neither RA nor SN
+    else #if weatherType was not RA or SN
     {
-      weatherData$snow_rain_days[day] = 0;   # set precip to 0
+      weatherData$snow_rain_days[day] = 0 #the value of snow_rain_days will be 0
     }
   }
 
 #add vector back to dataframe
 weatherData$snow_rain_days = as.character(weatherData$snow_rain_days)
+
+#make blow
+
+}
